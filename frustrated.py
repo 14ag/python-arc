92499a0088a66e49f3a0ec814e6012ad819e72b3
14ag <muriukipn@gmail.com>
2023-05-29 16:32:59 +0300

    29

--- frustrated.py
+++ frustrated.py
@@ -1,3 +1,12 @@
 w=float(input("first number: "))
 q=float(input("second number: "))
 print(str(q)+"÷"+str(w)+"="+str(q/w)) #print(q,"÷",w,"=",q/w,sep="")
+
+x = int(input())
+y = int(input())
+ 
+x = x % y
+x = x % y
+y = y % x
+ 
+print(y)
