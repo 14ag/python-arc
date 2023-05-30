5efeb754be8386f9a175bc4297d86aa7ebf4ebf7
14ag <muriukipn@gmail.com>
2023-05-30 13:41:06 +0300

    new

---
+++ snake_essentials.py
@@ -0,0 +1,6 @@
+# takes the parameter n as input integer
+input = int(input("Enter a number: "))
+# prints False if n is less than 100
+print(input >= 100)
+# True if n is greater than or equal to 100
+print(input >= 100)
