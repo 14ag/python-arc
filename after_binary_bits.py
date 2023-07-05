40ce32ba78aa01d01f948920941e3ebd0458f605
14ag <muriukipn@gmail.com>
2023-07-05 09:44:06 +0300

    5

--- after_binary_bits.py
+++ after_binary_bits.py
@@ -95,11 +95,13 @@ os.system('title lists')
 
 my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
 #
-# Write your code here.
+the=[]
+for i in range(my_list):
+    if i not in the:
+        the.append(i)        
 #
 print("The list with unique elements only:")
 print(my_list)
-
 
 
 
