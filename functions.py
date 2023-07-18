5676dcaa5e471efb1baf3899dd6f1160a56dd7a1
14ag <muriukipn@gmail.com>
2023-07-18 15:03:00 +0300

    18

--- functions.py
+++ functions.py
@@ -1,2 +1,41 @@
 import os
 os.system('cls')
+os.system('prompt $G')
+
+
+# def message():
+#     print("Enter a value: ")
+ 
+# print("We start here.")
+# message()
+# print("We end here.")
+
+
+
+# def x(the):
+#     var=int(input("Enter a value: "))
+#     print(the+"="+var)
+#     the=var
+# x(a)
+# x(b)
+# x(c)
+
+
+def message(number):
+    print("Enter a number:", number)
+ 
+number = 1234
+message(1)
+print(number)
+
+
+
+
+
+# def hello(name): # defining a function
+#     print("Hello,", name) # body of the function
+ 
+ 
+# name = input("Enter your name: ")
+ 
+# hello(name) # calling the function
