89bfdfd018d3d8f159256518a1788944bd7b7256
14ag <muriukipn@gmail.com>
2023-07-21 12:07:26 +0300

    21

--- after_binary_bits.py
+++ after_binary_bits.py
@@ -83,7 +83,7 @@ os.system('title lists')
 
 
 
-# =-=-=-=-=-=--=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=--=-=-=-=-==-=-=-=-=-=-=--=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=--=-=-=-=-==-=-=-=-=-=-=--=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=--=-=-=-=-==-=-
+# =-=-=-=-=-=--=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=--=-=-=-=-==-=-=-=-=-=-=--=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=--=-=-=-=-==-=-=-=-=-=-=--=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=--=-=-=-=-==-=-=-=-=-=-=-=-=--==-===-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=--==-=-=-=-
 
 # Imagine a list ‒ not very long, not very complicated, just a simple list containing some integer numbers. Some of these numbers may be repeated, and this is the clue. We don't want any repetitions. We want them to be removed.
 # Your task is to write a program which removes all the number repetitions from the list. The goal is to have a list in which all the numbers appear not more than once.
@@ -95,7 +95,11 @@ os.system('title lists')
 
 my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
 #
-# Write your code here.
+the=[]
+for i in my_list:
+    if i not in the:
+        the.append(i)
+my_list=the[:]
 #
 print("The list with unique elements only:")
 print(my_list)
