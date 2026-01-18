4a4056fb996d5b2416da10eb02a2c0741b0e8109
14ag <muriukipn@gmail.com>
2026-01-18 23:04:37 +0300

    inventory manager

--- 2026/currency_converter.py
+++ 2026/currency_converter.py
@@ -4,3 +4,10 @@ os.system('prompt $G')
 os.system('mode con: cols=60 lines=20')
 ##############################currency converter########################################
 
+print(" converts EUR/USD 1.08 ")
+def convertCurrency(amount,rate):
+    money= float(amount*rate-2)
+    if money < 0 : money=0 
+    return money
+
+print(convertCurrency(500,0.92))
---
+++ 2026/inventory_manager.py
@@ -0,0 +1,18 @@
+import os
+os.system('cls')
+os.system('prompt $G')
+os.system('mode con: cols=60 lines=20')
+##############################inventory manager########################################
+
+inventory = {"apples": 5, "bananas": 12, "oranges": 0}
+
+print(inventory)
+print("")
+shipment = ["apples", "oranges", "apples", "apples"]
+
+for i in shipment:
+    if i in inventory:
+        inventory[i]+=1
+        print(f"one {i} was added to the inventory")
+print("")
+print(inventory)
