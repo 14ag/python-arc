197e5e5b5739a4882b3e5582a2ae3522d6dbfa60
14ag <muriukipn@gmail.com>
2026-01-20 20:35:47 +0300

    fizzbuzz array

---
+++ 2026/fizzbuzz_array.py
@@ -0,0 +1,26 @@
+import os
+os.system('cls')
+os.system('prompt $G')
+os.system('mode con: cols=60 lines=20')
+##############################fizzbuzz array########################################
+
+#number= input("pick a number, any number: ")
+number=30
+def fizzBuzz(n):
+    n=int(n)+1
+    fizzbuzz=[]
+    for i in range(1,n):
+        if i%15==0:
+            fizzbuzz.append("FizzBuzz")
+            continue
+        if i%5==0:
+            fizzbuzz.append("Buzz")
+            continue
+        if i%3==0:
+            fizzbuzz.append("Fizz")
+            continue
+        else:
+            fizzbuzz.append(str(i))
+    return fizzbuzz                       
+
+print(fizzBuzz(number))
