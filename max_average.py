a8a71beda6331f6a63db3e09357e9e86a5eaa7e3
14ag <muriukipn@gmail.com>
2026-01-25 20:45:07 +0300

    max average

--- max_average.py
+++ max_average.py
@@ -5,21 +5,22 @@ os.system('mode con: cols=60 lines=20')
 ##############################maximum average subarray########################################
 
 def maxAvgSubarray(nums,k):
-    max_sum=sum([nums[:k]])
-    max_avg=float(max_sum)/k
+    sum0=sum(nums[:k])
+    max_avg=float(sum0)/k
 
-    for i in range(k,len(nums,1)):
-        curr_sum=max_sum-nums[i-k]+nums[k]
-        curr_avg=float(curr_sum)/k
+    for i in range(k,len(nums)):
+        sum0=sum0-nums[i-k]+nums[i]
+        
+        curr_avg=float(sum0)/k
 
-        if curr_avg>max_avg:
+        if curr_avg >max_avg:
             max_avg=curr_avg
-    return
+
+    return max_avg
 
 print(maxAvgSubarray([1, 12, -5, -6, 50, 3],4))
+
 print(maxAvgSubarray([5],1))
+
 print(maxAvgSubarray([0, 4, 0, 3, 2],2))
 
-
-#[0, 4, 0, 3, 2, 12, -5, -6, 50, 3]
-#0   1  2  3  4   5   6   7   8  9
